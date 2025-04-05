c286fb95a71de0e6c7ab3faff874a29faa9d6391
Krampus B. <tiobarryx@gmail.com>
2025-04-05 08:46:52 +0200

    Update loadCommonFunctions.js

--- src/utils/loadCommonFunctions.js
+++ src/utils/loadCommonFunctions.js
@@ -139,6 +139,11 @@ const sendBasuraReact = async () => {
     return await sendReply(`⚠️ Advertencia! ${text}`);
   };
 
+        const sendBasuraReact = async () => {
+    return await sendReact("🗑️");
+  };
+
+
   const sendWarningReact = async () => {
     return await sendReact("⚠️");
   };
